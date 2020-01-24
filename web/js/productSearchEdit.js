9f6eed7a129627da21ca1f5c8baab3066d9605eb
fujiharagiken <masahiro.fujihara2@gmail.com>
2020-01-24 15:36:22 +0900

    バグ修正

--- web/js/productSearchEdit.js
+++ web/js/productSearchEdit.js
@@ -11,96 +11,69 @@ $(function () {
                 
         // 明細情報取得
         var table = document.getElementById('searchTable');
-        var rows = table.rows.length; // 行数
-        
-        // パラメータ作成
-//        var parameterJson = [];
-        
-//        var test = document.getElementById('inputProductCode');
-        var test = document.getElementsByName('inputProductCode');
-        test.value = "";
+        var rows = table.rows.length;	    // 行数
+	var productCode = '';		    // 商品コード
+	var productName = '';		    // 商品名
+	var colorCode = '';		    // カラーコード
+	var price = '';			    // 価格
+	var stock = '';			    // 個数
 
-        // ラジオボタンが選択されている行のデータを抜き出す
-        for(var i=1; i<rows; i++){
-            var selFlg = table.rows[i].cells[0].children[0].checked;		// 選択フラグ
+	// ラジオボタンが選択されている行のデータを抜き出す
+	for(var i=1; i<rows; i++){
+	    var selFlg = table.rows[i].cells[0].children[0].checked;		// 選択フラグ
 
-	    if( selFlg == true) {
-		var productCode = table.rows[i].cells[1].innerText;		// 商品コード
-		var productName = table.rows[i].cells[1].innerText;		// 商品名
-//                var colorCode = table.rows[i].cells[3].children[0].value;     // カラーコード
-		var colorCode = table.rows[i].cells[4].innerText;		// カラーコード
-//		var price = Number(table.rows[i].cells[8].innerText.replace(/,/g, '').substring(1));;   // 価格
-		var price = Number(table.rows[i].cells[8].innerText);		// 価格
-		var stock = table.rows[i].cells[9].innerText;		// 個数
+	    if(selFlg) {
+		productCode = table.rows[i].cells[1].innerText;		// 商品コード
+		productName = table.rows[i].cells[1].innerText;		// 商品名
+		colorCode = table.rows[i].cells[4].innerText;		// カラーコード
+		price = Number(table.rows[i].cells[8].innerText);		// 価格
+		stock = table.rows[i].cells[9].innerText;		// 個数
 
-//		var productJson = {
-////		    selFlg : selFlg,
-//		    productCode : productCode,
-//		    productName : productName,
-//		    colorCode : colorCode,
-//		    price : price,
-//		    stock : stock
-//		};
-//		
-//                parameterJson[i-1] = productJson;
 		break;
 	    }
-        }
-         
+	}
+
         // 受注登録処理呼出
-        var form = document.createElement('form');
-        var request = document.createElement('input');
+	var form = document.createElement('form');
+	var request = document.createElement('input');
 
-        form.method = 'POST';
-        form.action = './orderEditPage.jsp';	    //仮　本当は変数で
-        request.type = 'hidden'; //入力フォームが表示されないように
+	form.method = 'POST';
+	form.action = './orderEditPage.jsp';	    //仮　本当は変数で
+	request.type = 'hidden'; //入力フォームが表示されないように
 
-//	request.name = 'parameterJson';
-//        request.value = JSON.stringify(parameterJson);
-//        
-//        "productCode"
-//        "productName"
-//        "colorCode"
-//        "stock"
-//        "price"
-
-	//投げるデータを作成する
+	//返却するデータを作成する
 	request.name = 'productCode';
 	request.value = productCode;
-        form.appendChild(request);
-        document.body.appendChild(form);
+	form.appendChild(request);
+	document.body.appendChild(form);
 
-	//投げるデータを作成する
+	//返却するデータを作成する
 	request.name = 'productName';
 	request.value = productName;
-        form.appendChild(request);
-        document.body.appendChild(form);
+	form.appendChild(request);
+	document.body.appendChild(form);
 
-	//投げるデータを作成する
+	//返却するデータを作成する
 	request.name = 'colorCode';
 	request.value = colorCode;
-        form.appendChild(request);
-        document.body.appendChild(form);
+	form.appendChild(request);
+	document.body.appendChild(form);
 
-	//投げるデータを作成する
+	//返却するデータを作成する
 	request.name = 'stock';
 	request.value = stock;
-        form.appendChild(request);
-        document.body.appendChild(form);
+	form.appendChild(request);
+	document.body.appendChild(form);
 
-	//投げるデータを作成する
+	//返却するデータを作成する
 	request.name = 'price';
 	request.value = price;
-        form.appendChild(request);
-        document.body.appendChild(form);
+	form.appendChild(request);
+	document.body.appendChild(form);
 
-        form.submit();
-        
-        test.value = JSON.stringify(parameterJson);
+	form.submit();
         
     });
-    
-    
 });
 
 
